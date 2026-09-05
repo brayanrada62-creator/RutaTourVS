1183b5d19164589038dbb76bb65ac034e7cffc5a
Valentina4Leonas <valentina4leonas@gmail.com>
2026-09-04 23:41:25 -0500

    Agrege componente panel administrativo

---
+++ frontend/rutatour/src/app/panel-administrativo/panel-administrativo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-administrativo',
+  imports: [],
+  templateUrl: './panel-administrativo.html',
+  styleUrl: './panel-administrativo.css',
+})
+export class PanelAdministrativo {
+
+}
