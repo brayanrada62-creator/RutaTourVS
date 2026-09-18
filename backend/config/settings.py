34ae28e8a13b98ff8666150937f682107ef0a9be
brayanrada62 <brayanrada62@gnal.com>
2026-09-17 21:47:57 -0500

    feat: implement

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-190(a!+07puz=&)dzzd(%hr&fhocg^00zir#nr9zc9#i_!tliv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
